e50382285f60be5155fa1c60f018bd5d07414d5b
Harshit Paneri <paneriharshit29@gmail.com>
2022-06-12 15:07:36 +0530

    move

---
+++ turtle02.py
@@ -0,0 +1,11 @@
+import turtle
+
+# Assign object of class turtle
+# turtle => class (small t)
+# Turtle => Object (capital T)
+t1=turtle.Turtle()
+# move forward by 50 units
+t1.forward(50)
+# right direction by 90 degree
+t1.right(90)
+t1.forward(50)
